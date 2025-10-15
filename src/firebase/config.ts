0926c5daa1207473be9dd8565f410fb12cb6d716
Pelumi owoeye <pelumiowoeye331@gmail.com>
2025-10-15 14:52:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4158950377-1e28f",
+  "appId": "1:949849825135:web:d42b590b1ad2f9ee7e129a",
+  "apiKey": "AIzaSyDqhCvCzLnezH5yrnnjZQ3VuY4nuO7aF48",
+  "authDomain": "studio-4158950377-1e28f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "949849825135"
+};
