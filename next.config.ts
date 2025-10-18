fb2d36bf851edc884ac73a8ba4d6426e3c0ca468
Pelumi owoeye <pelumiowoeye331@gmail.com>
2025-10-18 09:56:05 +0000

    my supabase credentials was in the .env.local folder before but you remo

--- next.config.ts
+++ next.config.ts
@@ -30,10 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  }
 };
 
 export default nextConfig;
