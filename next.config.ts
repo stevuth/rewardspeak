90e7d10b28949c866b78dd47c963f2664195d45b
Pelumi owoeye <pelumiowoeye331@gmail.com>
2025-10-18 10:48:54 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -95,6 +95,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**.playfull.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
