571b89cb1b3b6a0e103a5f095dd5e0b046d21a30
Pelumi owoeye <pelumiowoeye331@gmail.com>
2025-10-18 09:01:40 +0000

    do same to the close icon on the hamburger menu modal

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -69,9 +69,9 @@ const SheetContent = React.forwardRef<
     >
       {children}
       {!hideCloseButton && (
-        <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-            <X className="h-4 w-4" />
-            <span className="sr-only">Close</span>
+        <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-all hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground font-semibold text-secondary hover:text-primary hover:-translate-y-0.5">
+          <span className="text-xs uppercase tracking-wider">Close</span>
+          <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
       )}
     </SheetPrimitive.Content>
