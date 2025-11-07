5687cfeff8864b6741521c6349928f94e7589590
Pelumi owoeye <pelumiowoeye331@gmail.com>
2025-11-07 16:44:43 +0000

    reduce the size of the profile picture on the main nav bar on both devic

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import type { User as SupabaseUser } from "@supabase/supabase-js";
@@ -6,7 +5,7 @@ import { SafeImage } from "./safe-image";
 
 export function UserNav({ user, avatarUrl }: { user: SupabaseUser | null, avatarUrl: string | null }) {
   return (
-    <div className="relative h-16 w-16 rounded-full overflow-hidden">
+    <div className="relative h-12 w-12 rounded-full overflow-hidden">
         <SafeImage
             src={avatarUrl || `https://picsum.photos/seed/${user?.id || 'avatar'}/64/64`}
             alt="User avatar"
