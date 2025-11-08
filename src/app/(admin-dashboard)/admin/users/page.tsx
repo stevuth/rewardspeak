d180ba574250382c2f1a73f4e568be505a10b10c
Pelumi owoeye <pelumiowoeye331@gmail.com>
2025-11-08 02:09:55 +0000

    ReferenceError: TableRow is not defined     at ManageUsersPage (https://

--- src/app/(admin-dashboard)/admin/users/page.tsx
+++ src/app/(admin-dashboard)/admin/users/page.tsx
@@ -12,6 +12,8 @@ import {
 import {
   Table,
   TableBody,
+  TableCell,
+  TableRow,
 } from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
