4c7d3ac5093f280b8d4157e08a8cc419f530f30d
Pelumi owoeye <pelumiowoeye331@gmail.com>
2025-11-08 03:47:17 +0000

    there is a 'loading' text that shows at the top left corner of the websi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 
 import React from 'react';
 import { HomePageContent } from "@/app/home-page-content";
+import { WavingMascotLoader } from '@/components/waving-mascot-loader';
 
 
 export default async function Home() {
     return (
-        <React.Suspense fallback={<div>Loading...</div>}>
+        <React.Suspense fallback={
+            <div className="flex h-screen w-screen items-center justify-center">
+                <WavingMascotLoader text="Loading..." />
+            </div>
+        }>
           <HomePageContent />
         </React.Suspense>
     )
