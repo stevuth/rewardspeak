d6864ca595cbf7ac1dc5071419a811883d4791d4
Pelumi owoeye <pelumiowoeye331@gmail.com>
2025-11-19 14:49:23 +0000

    nextn@0.1.0 type-check > tsc --noEmit
    src/utils/supabase/server.ts:14:3

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -3,34 +3,46 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 import { revalidatePath } from 'next/cache';
 
+// NOTE: This is a server-only client.
+// It is used in Server Components, Server Actions, and Route Handlers.
+// It can be used to make both authenticated and unauthenticated requests.
+//
+// Because it is a server-only client, it can make use of the `cookies`
+// header from `next/headers` to securely manage user sessions.
 export function createSupabaseServerClient(revalidate: boolean = false) {
-  const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
+          const cookieStore = cookies();
           return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
           try { 
+            const cookieStore = cookies();
             cookieStore.set({ name, value, ...options });
             if (revalidate) {
                 revalidatePath('/', 'layout');
             }
           } catch (error) {
-            // This can happen in server components, safe to ignore.
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
           }
         },
         remove(name: string, options: CookieOptions) {
           try { 
+            const cookieStore = cookies();
             cookieStore.set({ name, value: '', ...options });
             if (revalidate) {
                 revalidatePath('/', 'layout');
             }
           } catch (error) {
-            // This can happen in server components, safe to ignore.
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
           }
         },
       },
