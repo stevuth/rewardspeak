09d4ecfa15d2207f6cc0f43e2342b3bdc58abdde
Pelumi owoeye <pelumiowoeye331@gmail.com>
2025-11-19 14:51:03 +0000

    Fix for src/utils/supabase/server.ts You need to await the cookies() cal

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -10,18 +10,17 @@ import { revalidatePath } from 'next/cache';
 // Because it is a server-only client, it can make use of the `cookies`
 // header from `next/headers` to securely manage user sessions.
 export function createSupabaseServerClient(revalidate: boolean = false) {
+  const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          const cookieStore = cookies();
           return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
           try { 
-            const cookieStore = cookies();
             cookieStore.set({ name, value, ...options });
             if (revalidate) {
                 revalidatePath('/', 'layout');
@@ -34,7 +33,6 @@ export function createSupabaseServerClient(revalidate: boolean = false) {
         },
         remove(name: string, options: CookieOptions) {
           try { 
-            const cookieStore = cookies();
             cookieStore.set({ name, value: '', ...options });
             if (revalidate) {
                 revalidatePath('/', 'layout');
--- workspace/src/utils/supabase/server.ts
+++ workspace/src/utils/supabase/server.ts
@@ -4,17 +4,18 @@ import { cookies } from 'next/headers';
 import { revalidatePath } from 'next/cache';
 
 export function createSupabaseServerClient(revalidate: boolean = false) {
-  const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
+          const cookieStore = cookies();
           return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
           try { 
+            const cookieStore = cookies();
             cookieStore.set({ name, value, ...options });
             if (revalidate) {
                 revalidatePath('/', 'layout');
@@ -25,6 +26,7 @@ export function createSupabaseServerClient(revalidate: boolean = false) {
         },
         remove(name: string, options: CookieOptions) {
           try { 
+            const cookieStore = cookies();
             cookieStore.set({ name, value: '', ...options });
             if (revalidate) {
                 revalidatePath('/', 'layout');
